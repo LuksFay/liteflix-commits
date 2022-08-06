70ad47b20fa3a1aa0f2b8ff3565207234df46125
Fernando Lucas Picco <lukspicco@hotmail.com>
2022-08-06 02:27:39 +0000

    Comenzando la maquetación

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+      <p>ORIGINAL DE <span>LITEFLIX</span></p>
+      <p>TITULO DE PELICULA</p>
+    </div>
+    <div>
+      <button>REPRODUCIR</button>
+      <button>MI LISTA</button>
+    </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+      <div>
+        <p>Liteflix</p>
+        <button>
+          Agregar Pelicula
+        </button>
+      </div>
+
+      <div>
+        <p>menu</p>
+        <p>notifications</p>
+        <p>profile</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
