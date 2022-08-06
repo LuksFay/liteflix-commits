12a19d7f42a75b47a4e3cb187bbf5b92b4a5deeb
Fernando Lucas Picco <lukspicco@hotmail.com>
2022-08-06 01:56:02 +0000

    conditional rendering

---
+++ src/README.md
@@ -0,0 +1,7 @@
+# Liteflix Challenge
+
+## Caso
+
+El proyecto consiste en el desarrollo un Catálogo de Películas dinámico.
+
+Se deberá listar una película destacada, y películas populares provenientes de una API pública.
---
+++ src/src/request.js
@@ -0,0 +1,12 @@
+const API_KEY = "6f26fd536dd6192ec8a57e94141f8b20";
+
+//Endpoints
+const request = {
+    peliculaDestacada: `/movie/now_playing?api_key=${API_KEY}` ,
+    populares:`/movie/popular?api_key=${API_KEY}`,
+    comedy: `/discover/movie?api_key=${API_KEY}&with_genres=35`
+}
+
+export default request
+
+//En el caso de que los endpoinst fueran mas, más tarde en el desarrollo sería mas facil hacer las request
